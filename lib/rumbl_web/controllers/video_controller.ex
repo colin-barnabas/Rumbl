e068f056d3e35f268e2b83ee0a05127f3788928c
Colin Barnabas <colin.barnabas@gmail.com>
2018-08-06 17:44:32 -0700

    Fix typo

--- lib/rumbl_web/controllers/video_controller.ex
+++ lib/rumbl_web/controllers/video_controller.ex
@@ -37,13 +37,13 @@ defmodule RumblWeb.VideoController do
   end
 
   def edit(conn, %{"id" => id}, current_user) do
-    video = Multimedia.get_video!(id)
-    changeset = Multimedia.change_video(conn.assigns.current_user, video)
+    video = Multimedia.get_user_video!(current_user, id)
+    changeset = Multimedia.change_video(current_user, video)
     render(conn, "edit.html", video: video, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "video" => video_params}) do
-    video = Multimedia.get_video!(id)
+  def update(conn, %{"id" => id, "video" => video_params}, current_user) do
+    video = Multimedia.get_user_video!(current_user,id)
 
     case Multimedia.update_video(video, video_params) do
       {:ok, video} ->
