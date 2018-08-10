98a6313acee3aa4b72f8b24fe109fe4ac183a618
Colin Barnabas <colin.barnabas@gmail.com>
2018-08-09 18:02:38 -0700

    Redirect back to index instead of show after create and update

--- lib/rumbl_web/controllers/video_controller.ex
+++ lib/rumbl_web/controllers/video_controller.ex
@@ -25,7 +25,7 @@ defmodule RumblWeb.VideoController do
       {:ok, video} ->
         conn
         |> put_flash(:info, "Video created successfully.")
-        |> redirect(to: video_path(conn, :show, video))
+        |> redirect(to: video_path(conn, :index))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
@@ -49,7 +49,7 @@ defmodule RumblWeb.VideoController do
       {:ok, video} ->
         conn
         |> put_flash(:info, "Video updated successfully.")
-        |> redirect(to: video_path(conn, :show, video))
+        |> redirect(to: video_path(conn, :index))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", video: video, changeset: changeset)
     end
